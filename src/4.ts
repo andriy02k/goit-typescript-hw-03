f3225f8f550f27170bdaaf272a3fca301e013101
andriy02k <krainyk02@gmail.com>
2024-01-29 22:14:08 +0200

    update

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,47 @@
-const key = new Key();
+class Key {
+    private signature: number = Math.random()
 
+    getSignature(): number {
+        return this.signature
+    }
+}
+
+class Person {
+    constructor(private key: Key) { }
+    
+    getKey(): Key {
+        return this.key
+    }
+}
+
+abstract class House {
+    protected door: boolean = false
+    protected tenants: Person[] = []
+    
+    constructor(protected key: Key) { }
+
+    comeIn(tenant: Person): void {
+        if (this.door) {
+            this.tenants.push(tenant)
+        }
+    }
+
+    openDoor(key: Key): void { }
+}
+
+class MyHouse extends House {
+    openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true
+        }
+    }
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
-house.openDoor(person.getKey());
-
-house.comeIn(person);
-
+const result = house.openDoor(person.getKey());
+const result1 = house.comeIn(person);
 
 export {};
